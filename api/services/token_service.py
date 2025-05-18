5699a247d8940663a7cf4280bdeac02a1c196417
ryhhill1998 <ryhhill1998@outlook.com>
2025-05-18 15:19:00 +0100

    removed test from token service

--- api/services/token_service.py
+++ api/services/token_service.py
@@ -1,8 +1,6 @@
 import jwt
 from cryptography.hazmat.primitives.asymmetric.rsa import RSAPrivateKey, RSAPublicKey
 
-from api.services.key_manager import KeyManager
-
 
 class TokenService:
     def __init__(self, private_key: RSAPrivateKey, public_key: RSAPublicKey, encryption_algorithm: str):
@@ -15,13 +13,3 @@ class TokenService:
 
     def decode_token(self, token: str):
         return jwt.decode(jwt=token, key=self.public_key, algorithms=[self.encryption_algorithm])
-
-
-km = KeyManager(keys_dir_path="../keys")
-keys = km.load_encryption_keys("2de07979-9888-4bb5-974e-5ba8b3c396cf")
-
-ts = TokenService(private_key=keys.private_key, public_key=keys.public_key, encryption_algorithm="RS256")
-token = ts.create_token({"test": "test"})
-print(f"{token = }")
-decoded = ts.decode_token(token)
-print(f"{decoded = }")
